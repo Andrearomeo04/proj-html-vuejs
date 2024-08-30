e61ca07c553d964aff0c764b7e8bc47a00ed9dc2
andrearomeo04 <pisco004@gmail.com>
2024-08-30 17:00:08 +0200

    [UPD] created first 3 section of main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,7 +7,7 @@ export default {
 <template>
     <div>
         <section>
-            <div class="bg-warning">
+            <div class="bg-warning p-5">
                 <div class="container">
                     <div class="row">
                         <div class="col-12">
@@ -17,9 +17,45 @@ export default {
                 </div>
             </div>
         </section>
+
+        <section class="mt-5">
+            <div class="container">
+                <div class="row g-0">
+                    <div class="col-4">
+                        <div class="content"></div>
+                    </div>
+                    <div class="col-8">
+                        <div class="content"></div>
+                    </div>
+                </div>
+                <div class="row g-0">
+                    <div class="col-6 offset-3 p-4">
+                        <div class="content"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="container">
+                <div class="row g-0">
+                    <div class="col-7">
+                        <div class="content"></div>
+                    </div>
+                    <div class="col-5">
+                        <div class="content"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
 <style lang="scss" scoped>
-    
+.content {
+        width: 100%;
+        height: 60px;
+        background-color: blueviolet;
+        border: 1px solid black;
+    }
 </style>
