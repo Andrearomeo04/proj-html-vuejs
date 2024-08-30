f483a1bb7eea9d5fee1c972926285884de62b79a
andrearomeo04 <pisco004@gmail.com>
2024-08-30 16:21:13 +0200

    [UPD] created header layout and created scss variables file

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,10 +6,28 @@ export default {
 
 <template>
     <div>
-        
+        <div class="container">
+            <div class="row g-0">
+                <div class="col-4">
+                    <div class="content"></div>
+                </div>
+                <div class="col-4">
+                    <div class="content"></div>
+                </div>
+                <div class="col-4">
+                    <div class="content"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-    
+    .content {
+        width: 100%;
+        height: 60px;
+        background-color: red;
+        border: 1px solid black;
+    }
+
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,7 +6,17 @@ export default {
 
 <template>
     <div>
-        
+        <section>
+            <div class="bg-warning">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="content"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
