2a4d0a3837b13d8605c72425595f998551c98ee9
andrearomeo04 <pisco004@gmail.com>
2024-08-30 15:44:34 +0200

    [FIX] Created macrostructure

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <script setup>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
 
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
 
 </script>
 
 <template>
   <div>
     <header>
-
+      <AppHeader />
     </header>
     <main>
-
+      <AppMain />
     </main>
     <footer>
-      
+      <AppFooter />
     </footer>
   </div>
 
 </template>
 
-<style scoped>
+<style lang="scss">
+@use './styles/generals.scss'
 
 </style>
