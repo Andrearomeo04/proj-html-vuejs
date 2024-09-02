f9d0ed22371937dbf50118c04dcc3faa433f6f06
andrearomeo04 <pisco004@gmail.com>
2024-09-02 04:37:20 +0200

    [UPD] added search button

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -15,8 +15,8 @@ export default {
 
 <template>
     <div>
-        <div class="container-fluid px-5">
-            <div class="row g-0 align-items-center py-2">
+        <div class="container-fluid py-2 px-header">
+            <div class="row g-0 align-items-center py-3">
                 <div class="col-4 d-flex">
                     <headerMenu 
                         v-for="(info, index) in mainMenu"
@@ -27,8 +27,17 @@ export default {
                 <div class="col-4 d-flex justify-content-center">
                     <img src="../assets/img/dark-logo.png" alt="dark-logo">
                 </div>
-                <div class="col-4">
-                    <div class="content"></div>
+                <div class="col-4 d-flex justify-content-end align-items-center">
+                    <div>
+                        <i class="fa-solid fa-cart-shopping mx-3"></i>
+                        <i class="fa-regular fa-circle-user mx-3"></i>
+                    </div>
+                    <div>
+                        <form class="d-flex" role="search">
+                            <input class="form-control" type="search" placeholder="Search..." aria-label="Search">
+                            <button class="btn" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -36,11 +45,8 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-    .content {
-        width: 100%;
-        height: 60px;
-        background-color: blueviolet;
-        border: 1px solid black;
+    .px-header {
+        padding: 0px 100px;
     }
 
     img {
