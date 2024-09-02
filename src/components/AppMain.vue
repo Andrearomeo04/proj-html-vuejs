e48776357370344e313f022caf6d6f4db4a50f16
andrearomeo04 <pisco004@gmail.com>
2024-09-02 05:06:44 +0200

    [UPD] created jumbo section and added fonts on _variables.scss

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,11 +8,13 @@ export default {
     <div>
         <!-- jumbo section -->
         <section>
-            <div class="bg-warning p-5">
+            <div class="jumbo p-5 d-flex justify-content-center align-items-center">
                 <div class="container">
                     <div class="row">
-                        <div class="col-12">
-                            <div class="content"></div>
+                        <div class="col-12 d-flex align-items-center flex-column text-white">
+                            <h2>Commence Business</h2>
+                            <h1 class="pb-4 font-10">Distant Mentoring Program</h1>
+                            <strong class="bg-white color-2 rounded px-5 py-3">Download free guidebook &#8594;</strong>
                         </div>
                     </div>
                 </div>
@@ -143,10 +145,25 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
 .content {
         width: 100%;
         height: 60px;
         background-color: blueviolet;
         border: 1px solid black;
+    }
+
+    .jumbo {
+        background-image: url(../assets/img/home-3-hero-bg.jpg);
+        background-size: cover;
+        min-height: 650px;
+    }
+
+    .color-2 {
+        color: $color-2;
+    }
+
+    .font-10 {
+        font-size: $font-10;
     }
 </style>
