2123e12508c2efb3ccedb6e9438b09c33d45d4bc
andrearomeo04 <pisco004@gmail.com>
2024-09-02 05:57:50 +0200

    [UPD]  learn about section almost finished

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -22,14 +22,20 @@ export default {
         </section>
 
         <!-- learn about section -->
-        <section class="mt-5">
+        <section class="margin-jumbo">
             <div class="container">
                 <div class="row g-0">
                     <div class="col-4">
-                        <div class="content"></div>
+                        <h6>EVERYTHING IN MAXCOACH</h6>
+                        <strong class="font-11">Learn About our <span class="color-2 fw-normal">Work Culture</span> at MaxCoach</strong>
+                        <p class="pb-4 pt-2">spend some time to visit our website or head office and discover our current courses, enrollment procedure, and registration deadline. We're opening new classes every beginning of each month.</p>
+                        <p>Have questions? <strong class="color-2">Get Free Guide &#8594;</strong></p>        
                     </div>
-                    <div class="col-8">
-                        <div class="content"></div>
+                    <div class="col-8 position-relative d-flex justify-content-end">
+                        <img src="../assets/img/stock-full-hd-10-ohki1ayywq3cqutettrncduujpoaorifqu4relt2ts.jpg" alt="stock-full-hd-10">
+                        <img src="../assets/img/about-us-01-ohkia3qg98rq9vg3s0mn2hkflr1migdqdw370qnegc.jpg" alt="">
+                        <img src="../assets/img/maxcoach-shape-02.png" alt="">
+                        <img src="../assets/img/maxcoach-shape-07.png" alt="">
                     </div>
                 </div>
                 <div class="row g-0">
@@ -159,8 +165,35 @@ export default {
         min-height: 650px;
     }
 
+    .margin-jumbo {
+        margin-top: 180px;
+    }
+
+    .col-8 {
+        img:nth-child(2) {
+            position: absolute;
+            top: -5rem;
+            right: -5rem;
+            border-radius: 5px;
+        }
+        img:nth-child(3) {
+            position: absolute;
+            bottom: -5rem;
+            right: 5rem;
+        }
+        img:nth-child(4) {
+            position: absolute;
+            top: -4rem;
+            z-index: -1;
+        }
+    }
+
     .color-2 {
         color: $color-2;
+    }
+
+    .font-11 {
+        font-size: $font-11;
     }
 
     .font-10 {
